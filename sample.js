444b895e78e01479aeb9098949702415a0cc7483
Jebin B V <jebin@avaamo.com>
2017-05-29 18:15:56 +0530

    Indentation with prettier, ESLint, Proxy support

--- sample.js
+++ sample.js
@@ -1,15 +1,19 @@
 const Avaamo = require("./index");
-const bot_uuid = "<YOUR_BOT_UUID>",
-	access_token = "<YOUR_BOT_ACCESS_TOKEN>";
+const bot_uuid = "<YOUR_BOT_UUID>", access_token = "<YOUR_BOT_ACCESS_TOKEN>";
 
-new Avaamo(bot_uuid, access_token, {
-	create_ticket: (response) => {
-		console.log("Request from designer", JSON.stringify(response));
-		return {
-			ticket_id: "My Ticket ID"
-		};
+new Avaamo(
+	bot_uuid,
+	access_token,
+	{
+		create_ticket: response => {
+			console.log("Request from designer", JSON.stringify(response));
+			return {
+				ticket_id: "My Ticket ID"
+			};
+		},
+		edit_ticket: response => {
+			return response;
+		}
 	},
-	edit_ticket: (response) => {
-		return response;
-	}
-}, true);
+	true
+);
