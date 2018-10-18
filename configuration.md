225e339f04b64aa0ad3d9c184cd78f21ffe5a612
david gwartney <david.gwartney@avaamo.com>
2018-10-18 17:31:55 -0500

    latest

---
+++ configuration.md
@@ -0,0 +1,1 @@
+Configuration of Hybrid SDK Purchase Requestion Server
---
+++ sql/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES(NULL, 'Norman', 'Peterson', 'accountant');
+INSERT INTO users VALUES(NULL, 'Sam', 'Malone', 'bartender');
+INSERT INTO users VALUES(NULL, 'Diane', 'Chamber', 'waitress');
+INSERT INTO users VALUES(NULL, 'Cliff', 'Clavin', 'postman');
